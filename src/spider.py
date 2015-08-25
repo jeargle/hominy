b64bc0e82eb36f3d9655b2504c46c238cc05146f
John Eargle <jeargle@gmail.com>
2015-08-24 23:36:59 -0500

    made command line parameter for URL

--- src/spider.py
+++ src/spider.py
@@ -5,19 +5,29 @@ import sys
 import json
 
 from bs4 import BeautifulSoup
+# import nltk
 import requests
 
 
-req = requests.get('http://www.python-requests.org/en/latest/')
-print req.status_code
-print req.headers
-print req.encoding
-html_doc = req.text
 
-soup = BeautifulSoup(html_doc, 'html.parser')
-# print soup.prettify()   # pretty print HTML
-print 'title:', soup.title
-for link in soup.find_all('a'):
-    print link.get('href')
 
-print soup.get_text()   # print page text
+if __name__=='__main__':
+
+    url = 'http://www.python-requests.org/en/latest/'
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    req = requests.get(url)
+    print req.status_code
+    print req.headers
+    print req.encoding
+    html_doc = req.text
+    
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    # print soup.prettify()   # pretty print HTML
+    print '*** Title:', soup.title
+    print '*** hrefs:'
+    for link in soup.find_all('a'):
+        print ' ', link.get('href')
+
+    print '*** Text:'
+    print soup.get_text()   # print page text
