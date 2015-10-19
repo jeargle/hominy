2e11215701b0b3b8db8e2bf0a309ab0bec6063df
John Eargle <jeargle@gmail.com>
2015-10-18 20:00:01 -0500

    getting user checks up and running

--- src/spider.py
+++ src/spider.py
@@ -10,33 +10,7 @@ import requests
 
 
 
-class RedditCheck:
-
-    def __init__(self, username):
-        self.username = username
-        self.url = 'http://www.reddit.com/user/' + username
-
-        
-class GithubCheck:
-
-    def __init__(self, username):
-        self.username = username
-        self.url = 'http://github.com/' + username
-
-
-class CrunchBaseCheck:
-
-    def __init__(self, name=None, org=None):
-        self.name = name
-        self.org = org
-        
-        if name is not None:
-            self.name_url = 'http://www.crunchbase.com/person/' + name + '#/entity'
-        if org is not None:
-            self.org_url = 'http://www.crunchbase.com/organization/' + org + '#/entity'
-
-        
-class Generic:
+class GenericUrl:
 
     def __init__(self, url):
         self.url = url
@@ -64,16 +38,70 @@ class Generic:
         # remove script elements
         [s.extract() for s in self.soup('script')]
 
-            
+
+class UrlCheck:
+
+    def __init__(self, url_base, user_suffix='%s', org_suffix='%s'):
+        self.url_base = url_base
+        self.user_suffix = user_suffix
+        self.org_suffix = org_suffix
+
+    def check_user(self, user):
+        if self.user_suffix is None:
+            return None
         
+        url = (self.url_base + self.user_suffix) % (user)
+        req = requests.get(url)
+        print 'url:', url
+        print 'status:', req.status_code
+        print 'headers:', req.headers
+        print 'encoding:', req.encoding
+
+    def check_org(self, org):
+        if self.org_suffix is None:
+            return None
+
+        url = (self.url_base + self.org_suffix) % (org)
+        req = requests.get(url)
+        print 'url:', url
+        print 'status:', req.status_code
+        print 'headers:', req.headers
+        print 'encoding:', req.encoding
+            
+
+class RedditCheck(UrlCheck):
+
+    def __init__(self):
+        UrlCheck.__init__(self, 'http://www.reddit.com/',
+                          user_suffix='user/%s')
+
+        
+class GithubCheck(UrlCheck):
+
+    def __init__(self):
+        UrlCheck.__init__(self, 'http://github.com/')
+
+
+class CrunchBaseCheck(UrlCheck):
+
+    def __init__(self, username=None, org=None):
+        UrlCheck.__init__(self, 'http://www.crunchbase.com/',
+                          user_suffix='person/%s#/entity',
+                          org_suffix='organization/%s#/entity')
+        
+
+
 if __name__=='__main__':
 
-    url = 'http://www.python-requests.org/en/latest/'
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
+    # url = 'http://www.python-requests.org/en/latest/'
+    # if len(sys.argv) > 1:
+    #     url = sys.argv[1]
 
-    g = Generic(url)
-    g.title()
-    g.hrefs()
-    g.kill_scripts()
-    g.text()
+    # g = GenericUrl(url)
+    # g.title()
+    # g.hrefs()
+    # g.kill_scripts()
+    # g.text()
+
+    rc1 = RedditCheck()
+    ghc1 = GithubCheck()
