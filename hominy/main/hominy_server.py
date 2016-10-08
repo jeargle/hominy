4759030a4c93982a8b599cb2f0a6256c946ec344
John Eargle <jeargle@gmail.com>
2016-10-07 20:04:56 -0500

    #10 fleshing out the base webserver

--- hominy/main/hominy_server.py
+++ hominy/main/hominy_server.py
@@ -13,18 +13,45 @@ from sqlalchemy.pool import NullPool
 from tornado import web, ioloop
 
 
+# Absolute paths
+app_path = os.path.split(os.path.abspath(__file__))[0]
+pkg_path = os.path.split(app_path)[0]
+hominy_path = os.path.split(pkg_path)[0]
+
+# Computed paths
+global_static = os.path.join(hominy_path, os.path.join('lib', 'www', 'static'))
+DOC_STATIC = os.path.join(hominy_path, 'doc', 'html')
+
+
+class AppHandler(web.RequestHandler):
+    @web.authenticated
+    @web.asynchronous
+    def get(self, app, mode):
+        if not mode:
+            mode = app
+        self.render(os.path.join(app, 'html', (mode + '.html')))
+
+        
 class IndexHandler(web.RequestHandler):
     def get(self):
         self.render('hominy.html')
 
 
+
+urls = [
+    (r'/', IndexHandler),
+    (r'/app/(.*)/(.*)', AppHandler),
+    (r'/static/(.*)', web.StaticFileHandler, {'path': '../../www/'}),
+    (r'/main/static/(.*)', web.StaticFileHandler, {'path': app_path}),
+]
+        
+
 app = web.Application(
-    [(r'/', IndexHandler),
-     (r'/static/(.*)', web.StaticFileHandler, {'path': '../../www/'}),
-     (r'/local_static/(.*)', web.StaticFileHandler, {'path': './'}),
-    ],
+    urls,
     debug = True
 )
+
+
 
 if __name__=='__main__':
     import logging
